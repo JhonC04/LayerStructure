4edcfead7d2456a77311edab2ba1fd6123b7e9bf
Jhon Carlos Leon Quiñones <cleon@cic.uis.edu.co>
2024-02-16 17:43:46 -0500

    Init

---
+++ Project.DataAccess/Models/Entities/TestChildTable.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccess
+{
+    public class TestChildTable
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("ForeignKey")]
+        public string IdParent { get; set; } = null!;
+        public double Doublevalue { get; set; }
+
+        #region Navigations properties
+            [ForeignKey("IdParent")]
+            public TestParentTable Parent { get; set; } = null!;
+        #endregion
+    }
+}
